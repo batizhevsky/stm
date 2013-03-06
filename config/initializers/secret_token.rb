65037684a598c2190960f07ab7c7480a0e7dc1cb
Leonid Batizhevsky <the.leonko@gmail.com>
2013-03-06 20:18:16 +0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTaskManager::Application.config.secret_token = '80df4b756d6782f02bfd005871718ab4dfad8034edf85996db017f6850296d4848b3bf7c3a2238cc23198d37d79fb46f4bb2ba1f3c8fbe0eda22ff5c3a2a0ecc'
