e458b88e1d517ebc01646e752d87c9b68f48941d
Leonid Batizhevsky <the.leonko@gmail.com>
2013-04-18 15:51:14 +0400

    eng locale

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,8 +1,20 @@
 class Web::ApplicationController < ApplicationController
   include FlashHelper
   include AuthHelper
+  before_filter :set_locale_from_params
+
+  protected
+
+  def set_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      end
+    end
+  end
 
   def default_url_options(options={})
-    { locale: params[:locale] || I18n.locale }
+    { locale: I18n.locale }
+
   end
 end
